fe4c0e7656085d028998e21fca3bf436893c1aac
GhidBase <grvdylans@gmail.com>
2024-12-08 00:21:45 -0700

    basic operations are functional in the browser

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 
-let currentInput;
-let lastInput;
-let operator;
+let currentInput = 0;
+let lastInput = null;
+let operator = null;
 
 function add() {
     operator = "add";
@@ -20,99 +20,63 @@ function divide() {
 }
 
 function operate() {
-    let a = lastInput;
-    let b = currentInput;
+    let a = +lastInput;
+    let b = +currentInput;
     switch (operator) {
-        case "divide":
+        case "/":
             return a / b;
-        case "multiply":
+        case "*":
             return a * b;
-        case "subtract":
+        case "-":
             return a - b;
-        case "add":
+        case "+":
             return a + b;  
     }
 }
 
-function numberPress(number) {
-    if(operator == null) {
-        currentInput += number.toString();
-        return;
-    }
-
-    if(operator == "=") {
-        lastInput = null;
-        operator = null;
+function numberPress(button) {
+    let number = button.textContent;
+    if(currentInput == 0) {
         currentInput = number;
+        console.clear();
+        console.log("lastInput: " + lastInput);
+        console.log("currentInput: " + currentInput);
         return;
     }
 
-    lastInput = currentInput;
-    currentInput = number;
+        currentInput += number.toString();
+        currentInput = +currentInput;
+        console.clear();
+        console.log("lastInput: " + lastInput);
+        console.log("currentInput: " + currentInput);
 }
 
-function operatorPressed(operatorInput) {
+function operatorPressed(button) {
+    let operatorInput = button.textContent;
     if (currentInput == null) {
         return;
     }        
 
     if (lastInput == null) {
         operator = operatorInput;
+        lastInput = currentInput;
+        currentInput = 0;
+        console.clear();
+        console.log("operator pressed " + button.textContent)
+        console.log("lastInput: " + lastInput);
+        console.log("currentInput: " + currentInput);
         return;
     }
 
     currentInput = operate();
 
+    lastInput = currentInput;
+    currentInput = 0;
     operator = operatorInput;
+
+
+    console.clear();
+    console.log("operator pressed " + button.textContent)
+    console.log("lastInput: " + lastInput);
+    console.log("currentInput: " + currentInput);
 }
-
-
-/*
-    The steps are like so
-    - Put in a currentInput
-    - Press an operator
-        - store the currentInput as the lastInput
-        - clear the currentInput
-    - type another currentInput
-
-    A.
-    - press equal
-        - currentInput = operate()
-
-    B.
-    - press another operator
-        - lastInput = operate()
-        - currentInput = null
-
-
-        HANDLING CLEARING AND NULL
-            Don't use null for current number. Always put 0. If choosing a number
-            when it's 0, overwrite 0 with the new number. Previous number can
-            be null
-
-        HANDLING NUMBER PRESSES
-            If a number is pressed with no queued operator, add a digit to
-            the number.
-
-            If a number is pressed while the operator is set to something other than "="
-            store the current number as previous number, and change the current number
-            to the pressed number
-
-            If the a number is pressed while the operator is set to "=", null the
-            previous number, null the operator, set the current number to the pressed
-            number
-        
-        HANDLING OPERATORS
-            If an operator is pressed while there is no previous input but there is
-            a current input, set the operator and keep the current input.
-
-            If an operator is pressed while there is a previous and current input
-            do the math for those two inputs based on the last chosen operator
-            store the result as the current input. Set the new operator
-
-        HANDLING THE EQUAL SIGN
-            If the equal sign is pressed while there is a set operator, solve the equation.
-            Set the operator to =
-
-        
-*/
